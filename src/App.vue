5ac5ecfac044bfb6094b8cde6921800b22b6bef2
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-22 09:56:58 -0300

    using pic api

--- src/App.vue
+++ src/App.vue
@@ -13,21 +13,17 @@
 export default {
   data(){
     return {
-
       titulo: 'alurapic',
-
-      fotos: [
-        {
-          url:'https://cdn.motor1.com/images/mgl/qjvEq/s1/2020-subaru-wrx-sti-render.webp',
-          titulo: 'subawuw'
-        },
-        {
-          url:'https://motorshow.com.br/wp-content/uploads/sites/2/2021/12/269057019-6617007225037554-3758995518522402541-n-e1640712075314.jpg',
-          titulo: 'subaruuuuu'
-        }
-      ]
+      fotos: []
     }
+  },
+  created(){
+    this.$http.get('http://localhost:3000/v1/fotos')
+    .then(res => res.json())
+    .then(fotos => this.fotos = fotos);
+
   }
+
 }
 </script>
 
