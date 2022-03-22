d9d736ed11b63ed4da37c6f8370199c482ededf5
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-22 11:29:06 -0300

    Lean scoped style

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -17,7 +17,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
   .painel {
     padding: 0 auto;
@@ -39,5 +39,10 @@ export default {
     padding: 10px;
     text-transform: uppercase;
   }
+
+    * {
+    box-shadow: 5px 5px 5px;
+  }
+
 
 </style>
