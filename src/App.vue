93474ebc172db01b274f317462b9db02fd4a6eab
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-22 11:09:30 -0300

    new component "painel"

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <div >
-    <h1>{{ titulo }}</h1>
-      <ul>
-        <li v-for="foto of fotos">
-          <img :src="foto.url" :alt="foto.titulo">
-        </li>
-      </ul>
+
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+    <ul class="lista-fotos">
+       <li class="lista-fotos-item" v-for="foto in fotos">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components:{
+    'meu-painel': Painel
+  },
+
   data(){
     return {
       titulo: 'alurapic',
       fotos: []
     }
   },
+
   created(){
     this.$http.get('http://localhost:3000/v1/fotos')
     .then(res => res.json())
-    .then(fotos => this.fotos = fotos);
+    .then(fotos => this.fotos = fotos, err=> console.log(err));
 
   }
 
@@ -28,5 +43,26 @@ export default {
 </script>
 
 <style>
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+    
+  .imagem-responsiva{
+    width: 100%;
+  }
 
 </style>
