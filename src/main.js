99818eb1f3b755f66b8087628b4d5120f896ac02
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-23 08:21:02 -0300

    feat: add Home.vue and add VueRoute

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
 Vue.use(VueResource);
-
+Vue.use(VueRouter);
 new Vue({
   el: '#app',
   render: h => h(App)
