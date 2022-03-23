8047fa9ac3bee9a57876dde8f199b263cfe4a135
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-23 09:53:22 -0300

    feat: add Menu.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <!-- Template -->
 <template>
+
   <div class="corpo">
 
-    <nav>
-      <!--ul>       
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/cadastro">Cadastro</router-link></li>
-      </ul-->
-      <ul>       
-          <li v-for="route in routes">
-            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
-          </li>
-      </ul>
-    </nav>
+    <meu-menu :rotas="routes"/>
+
     <transition name="pagina">
       <router-view></router-view>
     </transition>
+
   </div>
+
 </template>
 
 <!-- Script -->
 <script>
 
 import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
+
+  components:{
+    'meu-menu' : Menu
+  },
+
   data() {
     return {
       routes
