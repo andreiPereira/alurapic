239257e484637e5dab13fc7eed6110023a0276c2
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-23 09:27:02 -0300

    feat: add Menu route  and Transition

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <!-- Template -->
 <template>
   <div class="corpo">
-    <router-view></router-view>
+
+    <nav>
+      <!--ul>       
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/cadastro">Cadastro</router-link></li>
+      </ul-->
+      <ul>       
+          <li v-for="route in routes">
+            <router-link :to="route.path ? route.path : '/'">{{route.titulo}}</router-link>
+          </li>
+      </ul>
+    </nav>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <!-- Script -->
 <script>
 
-export default {
+import { routes } from './routes';
 
+export default {
+  data() {
+    return {
+      routes
+    }
+  }
 }
 
 </script>
@@ -21,6 +41,17 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+
+  .pagina-enter, 
+  .pagina-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .pagina-enter-active, 
+  .pagina-leave-active {
+    opacity: 0;
+    transform: scale(0.9);
   }
 
 </style>
