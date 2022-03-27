0eae490d1fad07737101f64d80a08eca2d97536f
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2022-03-27 13:45:21 -0300

    feat: rotate 90deg

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 
 Vue.directive('meu-transform', {
     bind(el, binding, vnode){
-        console.log('diretiva associada');
-        console.log(el);
+        
+        let current = 0;
+        el.addEventListener('dblclick', function(){
+            current+=90;
+            el.style.transform = `rotate(${current}deg)`;
+        });
     }
 });
